2c5b4a5dc6cd29dce6eb9dca976a275625ad5580
emilie-i-guess <emilidho@stud.ntnu.no>
2026-04-15 22:39:35 +0200

    fix: import models in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uuid
-import models
 import schemas
+from . import models
 from utils.utils import format_response
 from fastapi import FastAPI, Depends, HTTPException, Header
 from sqlalchemy.orm import Session
